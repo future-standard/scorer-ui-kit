1581f5b496e5884394ead19d62f1c89d87b2d7d3
Josh Lipps <josh.lipps@gmail.com>
2020-05-14 16:59:30 +0900

    change medium to font weight 500

--- src/Form/atoms/Label.tsx
+++ src/Form/atoms/Label.tsx
@@ -5,7 +5,7 @@ const StyledLabel = styled.label`
   color: hsl(207, 5%, 57%);
   font-size: 14px;
   font-family: Monorale;
-  font-weight: medium;
+  font-weight: 500;
   height: 16px;
   width: 135px;
   margin-bottom: 10px;
