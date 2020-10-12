a8509f67588070240ebc8959711a61ba8a9ec69d
Leonard Burton <leonard@atomworks.co>
2020-10-12 18:37:20 +0900

    Added knobs for DatePicker

--- storybook/src/stories/In Development/DatePicker.stories.tsx
+++ storybook/src/stories/In Development/DatePicker.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-// import {  text, select } from "@storybook/addon-knobs";
+import { boolean, select } from "@storybook/addon-knobs";
 import { DatePicker } from 'scorer-ui-kit';
 
 const Container = styled.div`
@@ -15,6 +15,9 @@ export default {
 
 export const _DatePicker = () => {
 
-  return <Container><DatePicker /></Container>;
+  const useTime = boolean("Use Time", true);
+  const mode = select("Mode", {single: "Single", interval: "interval"}, "interval");
+
+  return <Container><DatePicker {...{useTime, mode}} /></Container>;
 
 };
