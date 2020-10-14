cd96bc0113202f852c5ccd9da263e401084644d1
Leonard Burton <leonard@atomworks.co>
2020-10-14 15:51:04 +0900

    Moved out container for filter dropdown usage

--- storybook/src/stories/In Development/DatePicker.stories.tsx
+++ storybook/src/stories/In Development/DatePicker.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { select } from "@storybook/addon-knobs";
-import { DatePicker } from 'scorer-ui-kit';
+import { DatePicker, FilterDropdownContainer } from 'scorer-ui-kit';
 
 const Container = styled.div`
     margin: 20px;
@@ -18,6 +18,11 @@ export const _DatePicker = () => {
   const dateMode = select("Date Mode", {single: "single", interval: "interval"}, "interval");
   const timeMode = select("Time Mode", {off: "off", single: "single", interval: "interval"}, "interval");
 
-  return <Container><DatePicker {...{timeMode, dateMode}} /></Container>;
+  return <Container>
+    <FilterDropdownContainer>
+      <DatePicker {...{timeMode, dateMode}} />
+    </FilterDropdownContainer>
+  </Container>;
+
 
 };
