4fc184b16dbe01eafdfb81baa5360d5e3a4779aa
Leonard Burton <leonard@atomworks.co>
2020-11-04 13:40:41 +0900

    Updated Datepicker story for updated props

--- storybook/src/stories/In Development/DatePicker.stories.tsx
+++ storybook/src/stories/In Development/DatePicker.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { select } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
 import { DatePicker, FilterDropdownContainer } from 'scorer-ui-kit';
 
 const Container = styled.div`
@@ -18,9 +19,11 @@ export const _DatePicker = () => {
   const dateMode = select("Date Mode", {single: "single", interval: "interval"}, "interval");
   const timeMode = select("Time Mode", {off: "off", single: "single", interval: "interval"}, "interval");
 
+  const updateCallback = action('Date / Time Updated')
+
   return <Container>
     <FilterDropdownContainer>
-      <DatePicker {...{timeMode, dateMode}} />
+      <DatePicker {...{timeMode, dateMode, updateCallback}} />
     </FilterDropdownContainer>
   </Container>;
 
