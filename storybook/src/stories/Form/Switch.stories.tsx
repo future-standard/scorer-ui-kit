4b9ee75009ed7d881299495f94f54e6971f9eeb7
Leonard Burton <leonard@atomworks.co>
2020-12-07 17:42:22 +0900

    Made Switch consistent with Checkbox
    Includes most importantly being able to set the default checked state.

--- storybook/src/stories/Form/Switch.stories.tsx
+++ storybook/src/stories/Form/Switch.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import {  text, select } from "@storybook/addon-knobs";
+import { text, select, boolean } from "@storybook/addon-knobs";
 import {Switch} from 'scorer-ui-kit';
 
 export default {
@@ -12,10 +12,11 @@ export default {
 export const _Switch = () => {
 
   const labelText = text("Label Text", "The Label");
+  const checked = boolean("Default Checked", true)
   const state = select("State", { Default: "default", Disabled: "disabled", Locked: "locked", Loading: "loading", Failure: "failure" }, "default");
   const leftTheme = select("Left Theme", { Off: "off", On: "on", Danger: "danger" }, "off");
   const rightTheme = select("Right Theme", { Off: "off", On: "on", Danger: "danger" }, "on");
-  const onChange = action('value-changed');
+  const onChangeCallback = action('value-changed');
 
-  return <Switch {...{state, leftTheme, rightTheme, labelText, onChange}} />;
+  return <Switch {...{state, leftTheme, rightTheme, labelText, checked, onChangeCallback}} />;
 };
