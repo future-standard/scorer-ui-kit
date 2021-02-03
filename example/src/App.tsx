c0eb3806273671d9f4307e955f22a994e477164e
Isabel Anguiano <isabel.anguianop@gmail.com>
2021-02-03 16:42:16 +0900

    Add Global Notification as Example page

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -16,6 +16,7 @@ import LoginPage from './pages/Login';
 import TablePage from './pages/TablePage';
 import CustomUserDrawerPage from './pages/CustomUserDrawerPage';
 import TabsPage from './pages/TabsPage';
+import NotificationPage from './pages/NotificationPage';
 
 
 const App: React.FC<{}> = () => {
@@ -30,6 +31,7 @@ const App: React.FC<{}> = () => {
           <Route path={`/forms`} exact={true} component={FormPage}/>
           <Route path={`/table`} exact={true} component={TablePage}/>
           <Route path={`/customdrawer`} exact={true} component={CustomUserDrawerPage}/>
+          <Route path={`/notification`} exact={true} component={NotificationPage} />
           <Route path={`/ptz`} exact={true}>
             <PTZProvider socketUrl='ws://localhost/wsapp/' imageRefresh={2000}>
               <PTZPage/>
---
+++ example/src/pages/NotificationPage.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Button, useNotification } from 'scorer-ui-kit';
+import { INotificationProps } from '../../../dist/Alerts/atom/Notification';
+
+
+const Container = styled.div`
+  margin: 100px 20px 20px 20px;
+  display: grid;
+  grid-template-columns: 600px;
+  row-gap: 15px;
+`;
+
+const NotificationPage : React.FC = ({}) => {
+  
+  const {sendNotification } = useNotification(); 
+
+  const processAtClose = () => {
+    console.log('I have succesfully deliver the message mate');
+  }
+
+  const redirectAtClose = () => {
+    window.open('https://nihongoipsum.com/', '_blank');
+  }
+
+  const notiInfoSettings : INotificationProps = {
+    type: 'info',
+    message: 'You have pending messages in your mailbox',
+    closeCallback: () => processAtClose(),
+  }
+
+  const notiWarningSettings : INotificationProps = {
+    type: 'warning',
+    message: 'Those settings might be very expensive. You can learn about different settings',
+    actionTextButton: 'SHOW DOCUMENTATION',
+    closeCallback: () => redirectAtClose(),
+  }
+
+  const notiErrorSettings : INotificationProps = {
+    type: 'error',
+    message: 'There was a problem starting that process consult support',
+    closeCallback: () => processAtClose(),
+  }
+
+  const notiSucessSettings : INotificationProps = {
+    type: 'success',
+    message: 'You have sucessfully bought a new camera',
+    autoDismiss: true,
+  }
+
+  const notiNeutralSettings : INotificationProps = {
+    type: 'neutral',
+    message: 'Try our new pinaple pizza',
+    closeCallback: () => processAtClose(),
+  }
+
+
+  return(
+    <Container>
+      <Button
+        onClick = {() => sendNotification(notiInfoSettings)}
+      >Send information notification</Button>
+            <Button
+        onClick = {() => sendNotification(notiSucessSettings)}
+      >Send success with auto dismiss</Button>
+      <Button
+        onClick = {() => sendNotification(notiWarningSettings)}
+      >Send warning notification with action</Button>
+      <Button
+        design = 'danger'
+        onClick = {() => sendNotification(notiErrorSettings)}
+      >Send Error notification</Button>
+    </Container>
+  );
+}
+
+export default NotificationPage;
