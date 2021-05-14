f8de4b6f2c52f21fafec1b1cbbefa0193dd2360b
Isabel Anguiano <isabel.anguianop@gmail.com>
2021-05-14 12:18:39 +0900

    Remove Notification Page from Examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -17,7 +17,6 @@ import LoginPage from './pages/Login';
 import TablePage from './pages/TablePage';
 import CustomUserDrawerPage from './pages/CustomUserDrawerPage';
 import TabsPage from './pages/TabsPage';
-import NotificationPage from './pages/NotificationPage';
 
 const App: React.FC<{}> = () => {
   return (
@@ -32,7 +31,6 @@ const App: React.FC<{}> = () => {
         <Route path={`/forms`} exact={true} component={FormPage} />
         <Route path={`/table`} exact={true} component={TablePage} />
         <Route path={`/customdrawer`} exact={true} component={CustomUserDrawerPage} />
-        <Route path={`/notification`} exact={true} component={NotificationPage} />
         <Route path={`/ptz`} exact={true}>
           <PTZProvider socketUrl='ws://localhost/wsapp/' imageRefresh={2000}>
             <PTZPage />
