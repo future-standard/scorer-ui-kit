165401eaa624a5b9040a9555be355cf3a0b8f1df
Josh Lipps <josh.lipps@gmail.com>
2021-07-08 18:15:05 +0900

    Add theme fallback helper (#133)

--- src/themes/dark.tsx
+++ src/themes/dark.tsx
@@ -4,6 +4,35 @@ const colors = {
 
 };
 
+const styles = {
+  "form": {
+    "button": {
+      "primary": {
+          "default": {
+              // "backgroundImage": "linear-gradient(135.00deg, blue 0%, green 100%)"
+
+              "backgroundImage": "linear-gradient(135.00deg, hsla(200, 77.5%, 68.6%, 1.000) 0%, hsla(207, 80.2%, 64.3%, 1.000) 100%)"
+          },
+          // "hover": {
+          //     "backgroundColor": "hsla(207, 88.2%, 60%, 1.000)"
+          // },
+          // "active": {
+          //     "backgroundColor": "hsla(195, 79.5%, 71.4%, 1.000)"
+          // },
+          // "divider": {
+          //     "backgroundColor": "hsla(202, 70.4%, 62.9%, 1.000)"
+          // },
+          // "disabled": {
+          //     "backgroundImage": "linear-gradient(135.00deg, hsla(200, 77.5%, 68.6%, 1.000) 0%, hsla(207, 80.2%, 64.3%, 1.000) 100%)"
+          // },
+          // "actionArea": {
+          //     "backgroundImage": "linear-gradient(135.00deg, hsla(200, 77.5%, 68.6%, 1.000) 0%, hsla(207, 80.2%, 64.3%, 1.000) 100%)"
+          // }
+      }
+    }
+  }
+};
+
 const tmp = {
   input: {
     states: {
@@ -65,6 +94,7 @@ export default {
   dimensions,
   colors,
   tmp,
+  styles,
   animation,
   deviceSize,
   deviceMediaQuery,
