5466d912ff834c92b3782855e5f5a89f1bcbea45
fti-dshelke <dshelke@ftilab.com>
2022-04-20 13:59:25 +0530

    Make component as FC

--- example/src/pages/GlobalUIPage.tsx
+++ example/src/pages/GlobalUIPage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { FC } from "react";
 import { GlobalUI } from "scorer-ui-kit";
 
-const GlobalUIPage = () => {
+const GlobalUIPage: FC = () => {
   return (
     <GlobalUI
       accountOptionText="Account Options"
