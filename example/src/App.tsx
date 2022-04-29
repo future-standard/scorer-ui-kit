fe654ff10c76b7d879c3d3229f55dcf701ad622d
fti-pvishwakarma <pvishwakarma@ftilab.com>
2022-04-29 09:56:10 +0530

    Remove datepicker from example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -17,7 +17,6 @@ import LoginPage from './pages/Login';
 import TablePage from './pages/TablePage';
 import CustomUserDrawerPage from './pages/CustomUserDrawerPage';
 import TabsPage from './pages/TabsPage';
-import DatePickerPage from './pages/DatePickerPage';
 
 const App: React.FC<{}> = () => {
   return (
@@ -38,7 +37,6 @@ const App: React.FC<{}> = () => {
           </PTZProvider>
         </Route>
         <Route path={`/tabs`} exact={true} component={TabsPage} />
-        <Route path='/datepicker' exact component={DatePickerPage} />
       </Switch>
     </Router>
   )
