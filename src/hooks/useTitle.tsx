083b46efdc72e65a4eff00e61009994135b3bb80
Isabel Anguiano <isabel.anguianop@gmail.com>
2022-05-25 19:11:18 +0900

    Fix overlaping of titles and add hideAreaInDocTitle option

--- src/hooks/useTitle.tsx
+++ src/hooks/useTitle.tsx
@@ -1,17 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export const useTitle = (title : string, area? : string, delimiter?: string, update= true) => {
 
-  const [baseTitle] = useState<string>(document.title);
 
   useEffect(() => {
     if(!update) return;
     document.title = makeTitle([
       ...(title ? [title] : []),
-      ...(area ? [area] : []),
-      baseTitle
+      ...(area ? [area] : [])
     ], delimiter);
-  },[title, area, baseTitle, delimiter, update]);
+  },[title, area, delimiter, update]);
 };
 
 /**
