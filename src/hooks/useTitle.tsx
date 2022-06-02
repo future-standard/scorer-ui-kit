45ffb48200bfd5d583e4934f1e22d416f1fd1dcb
Isabel Anguiano <isabel.anguianop@gmail.com>
2022-06-02 17:50:32 +0900

    Return Base Title to have index title always

--- src/hooks/useTitle.tsx
+++ src/hooks/useTitle.tsx
@@ -1,18 +1,31 @@
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export const useTitle = (title : string, area? : string, delimiter?: string, update= true) => {
 
+// Index Title
+const baseTitle = document.title.split('|').slice(-1)[0].trim();
+const location = useLocation();
+
+// on unmount will return the baseTitle to Index Title for pages not using this hook.
+useEffect(() => {
+  return () => {
+    document.title = baseTitle;
+  };
+}, [baseTitle, location.pathname]);
 
   useEffect(() => {
     if(!update) return;
     document.title = makeTitle([
       ...(title ? [title] : []),
-      ...(area ? [area] : [])
+      ...(area ? [area] : []),
+      ...(baseTitle? [baseTitle] : [])
     ], delimiter);
-  },[title, area, delimiter, update]);
+
+  },[title, area, delimiter, update, baseTitle]);
 };
 
-/**
+/**useT
  * Put the parts together for title use.
  * @param parts Each part to make up the title.
  * @param delimiter Character used for delimiting
