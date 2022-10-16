26dd5d82532c87ad501da0793b291104b8f7acfc
fti-pfarne <pfarne@ftilab.com>
2022-10-16 19:40:24 +0530

    Add css for user detail component

--- src/Global/atoms/UserDetails.tsx
+++ src/Global/atoms/UserDetails.tsx
@@ -1,63 +1,72 @@
 import React, { useRef, useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css }  from 'styled-components';
 import { IUserDetails } from '..';
 import Icon from '../../Icons/Icon';
-import { Label } from '../../Form';
 
 const ExampleTitle = styled.div`
-  min-height: 48px;
   margin: 10px 10px;
   border: 2px solid #e2e2e380;
   background-color: #eeeded80;
   border-radius: 5px;
+  &:active { 
+    box-shadow: 4px 4px #eeeded80;;
+  }
 `;
 
-const LabelTitle = styled(Label)`
+const LabelTitle = styled.div`
   font-size: 12px; 
-  padding-top: 5px;
-  margin-bottom: -4px;
   padding-left: 10px;
-  font-weight: 600;
+  font-weight: 800;
   max-width: 200px;
   overflow-wrap: initial;
   white-space: break-spaces;
   overflow: hidden;
   text-overflow: ellipsis;
+  color: #585858e6;
+  opacity: 0.7;
 `;
 
-const LabelContent = styled(Label)`
+const LabelContent = styled.div`
   font-size: 12px; 
-  padding-bottom: 5px;
   padding-left: 10px;
   max-width: 200px;
   overflow-wrap: initial;
   white-space: break-spaces;
   overflow: hidden;
   text-overflow: ellipsis;
+  margin: 5px 0px;
+  font-weight: 700;
+  color: #585858e6;
+  opacity: 0.7;
 `;
 
-const LabelNotes = styled(Label)`
+const LabelNotes = styled.div`
   font-size: 10px;
-  padding-bottom: 5px;
   padding-left: 10px;
   max-width: 200px;
-  overflow-wrap: initial;
-  white-space: break-spaces;
+  white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  margin-top: -25px;
-  margin-bottom: -7px;
-  overflow-wrap: anywhere;
+  margin: 0 0 5px 0;
+  font-weight: 700;
+  color: #585858e6;
+  opacity: 0.7;
+  max-height: 23px;
 `;
 
 const TitleIcon = styled(Icon)`
-  margin-top: 5px;
-  margin-left: 10px;
 `;
 
 const TitleContainer = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
+  margin-left: 10px;
+  margin: 3px 0 3px 10px;
+`;
+
+const Container = styled.div`
+  cursor: pointer;
 `;
 
 interface IProps {
@@ -82,16 +91,20 @@ const UserDetails : React.FC<IProps> = ({item, contextKey, onClickCallback, onUs
   }, [refSubmenu, setSubmenuHeight, contextKey]);
 
   return (
-    <div onClick={onUserDetailsClick}>
-      <ExampleTitle>
-        <TitleContainer>
-          <TitleIcon icon={icon} size={12} />
-          <LabelTitle title={title}  htmlFor='' labelText={title as string}/>
-        </TitleContainer>
-        <LabelContent title={subTitle}  htmlFor='' labelText={subTitle as string} />
-        <LabelNotes title={notes} htmlFor='' labelText={notes as string} />
-      </ExampleTitle>
-    </div>
+      <Container onClick={onUserDetailsClick}>
+        <ExampleTitle>
+          <TitleContainer>
+            <TitleIcon icon={icon} size={12} />
+            <LabelTitle title={title}>{title}</LabelTitle>
+          </TitleContainer>
+          {subTitle !=='' ?
+            <LabelContent title={subTitle}>{subTitle}</LabelContent>
+          : null}
+          {notes !=='' ?
+            <LabelNotes title={notes}>{notes}</LabelNotes>
+          : null}
+        </ExampleTitle>
+      </Container>
   );
 };
 
