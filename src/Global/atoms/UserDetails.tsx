8e9ad82bd338187391acdb7f5ed76dbbff6fb6c6
fti-pfarne <pfarne@ftilab.com>
2022-10-17 13:29:23 +0530

    Change in margin

--- src/Global/atoms/UserDetails.tsx
+++ src/Global/atoms/UserDetails.tsx
@@ -50,7 +50,7 @@ const LabelNotes = styled.div`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  margin: 3px 0 5px 0;
+  margin: 4px 0 4px 0;
   font-weight: 700;
   color: #585858e6;
   opacity: 0.7;
