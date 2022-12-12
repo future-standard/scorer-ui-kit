54fdd34e04896141cba1d77e666a164907f52ed0
fti-smaske <smaske@ftilab.com>
2022-12-12 16:02:02 +0530

    Update Component name

---
+++ storybook/src/stories/Filters/molecules/FilterLayout.stories.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
+import { FilterLayout } from 'scorer-ui-kit';
+import { select } from '@storybook/addon-knobs';
+
+export default {
+  title: 'Filters/molecules',
+  component: FilterLayout,
+  decorators: []
+};
+
+const Container = styled.div``;
+
+export const _FilterLayout = () => {
+  const language = select("Language", { English: 'english', Japanese: "japanese" }, "japanese");
+  const sizeOptions = [10, 20, 50, 100];
+  const [pageSize, setPageSize] = useState(20);
+
+  const onSizePerPageChange = useCallback((size: number) => {
+    setPageSize(size);
+  },[]);
+
+  const handleLayout = useCallback((layout: string) => {
+    
+  }, []);
+
+  return (
+    <Container>
+      <FilterLayout
+      {...{
+        onPageSizeChange: onSizePerPageChange,
+        pageSizeOptions: sizeOptions,
+        defaultPageSize: pageSize,
+        getLayout: handleLayout,
+        buttonValue1: 'grid',
+        buttonValue2: 'list',
+        icon1: 'LayoutGrid',
+        icon2: 'LayoutList',
+        layoutText: language === 'english' ? 'Layout' : 'レイアウト',
+        pageSizeText: language === 'english' ? 'Items Per Page' : '１ページのアイテム数'
+      }}
+      />
+    </Container>
+  )
+}
