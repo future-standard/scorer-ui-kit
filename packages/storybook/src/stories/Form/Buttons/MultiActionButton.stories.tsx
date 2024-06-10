0bfd8ebc23c5ee7895bc37f1c099ad96bdc1373f
Isabel Anguiano <isabel.anguianop@gmail.com>
2024-06-10 14:29:56 +0900

    Add Multi Action Button Story

---
+++ packages/storybook/src/stories/Form/Buttons/MultiActionButton.stories.tsx
@@ -0,0 +1,27 @@
+import { boolean, select } from "@storybook/addon-knobs";
+import React from "react";
+import { MultiActionButton } from 'scorer-ui-kit';
+
+export default {
+  title: 'Form/Buttons',
+  component: MultiActionButton,
+  decorators: []
+};
+
+const buttonList = [
+  {id: 'a0', text: 'Example Action', icon: 'Success',  onClick: () => {console.log('Example Action pressed') } },
+  {id: 'a1', text: 'SuperCalifragilisticoEspialidoso', icon: 'Add', onClick: () => {console.log('SuperCalifragilisticoEspialidoso pressed') } },
+  {id: 'a2', text: 'Example Action 2', icon: 'Success', isLoading:true , onClick: () => {console.log('Action 2 pressed') } },
+  {id: 'a3', text: 'Example Action 3', icon: 'Success', onClick: () => {console.log('Action 3 pressed')}, disabled:true  },
+]
+
+
+export const _MultiActionButton = () => {
+
+  const buttonDesign = select("Design", { Primary: "primary", Secondary: "secondary", Danger: "danger", Custom: "custom" }, "primary");
+  const buttonDisabled = boolean("Disabled", false);
+  const buttonSize = select("Size", { Xsmall: 'xsmall', Small: "small", Normal: "normal", Large: "large" }, "normal");
+
+
+  return <MultiActionButton activeId={"a1"} design={buttonDesign} size={buttonSize} disabled={buttonDisabled} buttonList={buttonList}>Button</MultiActionButton>
+}
