a1bcbf445131eaa601a3fc2a7ec7d27fd4e6276c
Isabel Anguiano <isabel.anguianop@gmail.com>
2024-06-20 10:58:30 +0900

    Add an option in japanese to the MultiActionButton story

--- packages/storybook/src/stories/Form/Buttons/MultiActionButton.stories.tsx
+++ packages/storybook/src/stories/Form/Buttons/MultiActionButton.stories.tsx
@@ -10,9 +10,9 @@ export default {
 
 const buttonList = [
   {id: 'a0', text: 'Example Action', icon: 'Success',  onClick: () => {console.log('Example Action pressed') } },
-  {id: 'a1', text: 'SuperCalifragilisticoEspialidoso', icon: 'Add', onClick: () => {console.log('SuperCalifragilisticoEspialidoso pressed') } },
-  {id: 'a2', text: 'Example Action 2', icon: 'Success', isLoading:true , onClick: () => {console.log('Action 2 pressed') } },
-  {id: 'a3', text: 'Example Action 3', icon: 'FilterAscending', onClick: () => {console.log('Action 3 pressed')}, disabled:true  },
+  {id: 'a1', text: '日本語の場合はランダム', icon: 'Add', onClick: () => {console.log('日本語の場合はランダム') } },
+  {id: 'a2', text: 'Example Action 2', icon: 'Save', isLoading:true , onClick: () => {console.log('Action 2 pressed') } },
+  {id: 'a3', text: 'Example Action 3', icon: 'Download', onClick: () => {console.log('Action 3 pressed')}, disabled:true  },
 ]
 
 
@@ -23,5 +23,5 @@ export const _MultiActionButton = () => {
   const buttonSize = select("Size", { Small: "small", Normal: "normal", Large: "large" }, "normal");
   const list = object("Button List", buttonList);
 
-  return <MultiActionButton activeId={"a1"} design={buttonDesign} size={buttonSize} disabled={buttonDisabled} buttonList={list}>Button</MultiActionButton>
+  return <MultiActionButton activeId={"a0"} design={buttonDesign} size={buttonSize} disabled={buttonDisabled} buttonList={list}>Button</MultiActionButton>
 }
