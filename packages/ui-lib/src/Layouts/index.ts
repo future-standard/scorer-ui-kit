ef728643bf35aa5958d707d37558775ae3fa06e5
Leonard Burton <leonard.burton@futurestandard.co.jp>
2024-07-18 14:08:14 +0900

    Ported types used across multiple components

--- packages/ui-lib/src/Layouts/index.ts
+++ packages/ui-lib/src/Layouts/index.ts
@@ -1,4 +1,18 @@
-import ContentLayout from './ContentLayout';
+import ContentLayout from './organisms/ContentLayout';
+
+interface IBreadcrumb {
+  text: string;
+  href: string;
+}
+
+export interface IUtilityHeader {
+  $iconInGutter?: boolean;
+  backLink?: string;
+  showBreadcrumbs?: boolean;
+  breadcrumbs?: IBreadcrumb[];
+  showShareLink?: boolean;
+  shareLink?: string;
+}
 
 export interface IHeaderContent {
   PageHeader?: React.ReactNode | React.FC;
