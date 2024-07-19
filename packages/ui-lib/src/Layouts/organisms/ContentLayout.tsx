1c8c285b85dda7f4c6c9b480dc8453a14de385f0
Leonard Burton <leonard.burton@futurestandard.co.jp>
2024-07-19 18:46:07 +0900

    Container is now centered when max-width reached

--- packages/ui-lib/src/Layouts/organisms/ContentLayout.tsx
+++ packages/ui-lib/src/Layouts/organisms/ContentLayout.tsx
@@ -4,43 +4,50 @@ import UtilityHeader from "../molecules/UtilityHeader";
 import { IHeaderContent } from "..";
 import { Tabs } from '../../Tabs/Tabs';
 
-
-
-const breakToFullWidth = css`
-  // Declared seperately as it breaks styled components otherwise.
-  margin-left: calc(var(--content-layout-padding-left) * -1);
-  margin-right: calc(var(--content-layout-padding-right) * -1);
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 `
 
-const Container = styled.div``
-
-const FullWidthSubcontainer = styled.div`
-  ${breakToFullWidth};
-` 
 const HeaderArea = styled.div`
+  width: 100%;
+  max-width: var(--max-content-width);
   padding: 
     24px
     var(--content-layout-padding-right)
     32px
     var(--content-layout-padding-left);
+  
 `
 const TabArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  border-bottom: 1px solid var(--grey-8);
+`
+
+const TabAreaInner = styled.div`
+  max-width: var(--max-content-width);
+  flex: 1;
+  width: 100%;
   padding: 
     0
     var(--content-layout-padding-right)
     0
     var(--content-layout-padding-left);
-
-  border-bottom: 1px solid var(--grey-8);
 `
-const TabAreaInner = styled.div``
   
 const Content = styled.div`
+  width: 100%;
+  max-width: var(--max-content-width);
   padding: 
     var(--content-layout-padding-top)
     var(--content-layout-padding-right)
     var(--content-layout-padding-bottom)
     var(--content-layout-padding-left);
+  
 `
 
 interface IContentLayout {
