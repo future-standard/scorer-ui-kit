73d43db81e979b4ac601df2a983de195f9496f3f
Leonard Burton <leonard.burton@futurestandard.co.jp>
2024-07-30 17:49:03 +0900

    Using new component for placeholder data

--- packages/example/src/pages/SplitLayout.tsx
+++ packages/example/src/pages/SplitLayout.tsx
@@ -1,88 +1,18 @@
-import React, { FC, useState, useRef } from "react";
-import styled from "styled-components";
+import React, { FC, useState } from "react";
 import { ThemeProvider } from 'styled-components';
-import { GlobalUI, defaultTheme, useThemeToggle, ContentLayout, SplitLayout, ISplitLayoutHandles } from "scorer-ui-kit";
-import { IHeaderContent } from "scorer-ui-kit/dist/Layouts";
-
-const Arrow = styled.div`
-    position: absolute;
-    width: 10px;
-    height: 10px;
-    border: 2px solid var(--grey-10);
-    border-left: 0;
-    border-bottom: 0;
-    border-radius: 0 3px 0 0;
-
-
-    &.t { top: 24px; }
-    &.b { bottom: 24px; }
-    &.l { left: 24px; }
-    &.r { right: 24px; }
-  `
-
-  const ContentPlaceholder = styled.div`
-    display: flex;
-    flex: 1;
-    height: 100%;
-    background: var(--grey-3);
-    border: 12px solid var(--grey-6);
-    box-sizing: border-box;
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    letter-spacing: 1.2px;
-    color: var(--grey-10);
-  `
-
-  const FloatingDemoControls = styled.div`
-    position: fixed;
-    z-index: 10;
-    top: 20px;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    gap: 8px;
-
-    button {
-      background: #fff9;
-      border: 1px solid #aaa;
-      text-transform: uppercase;
-      font-size: 12px;
-      padding: 4px 8px;
-      letter-spacing: 0.2px;
-      border-radius: 3px;
-      cursor: pointer;
-      &:hover {
-        background: #fff;
-        box-shadow: 0 2px 2px #0001;
-      }
-      &:active {
-        box-shadow: 0 -2px 2px #00000005;
-        background: #fff4;
-      }
-    }
-  `
+import { GlobalUI, defaultTheme, useThemeToggle, ContentLayout, SplitLayout, FlexContentPlaceholder } from "scorer-ui-kit";
 
 const SplitLayouts: FC = () => {
 
   const {onThemeToggle, isLightMode} = useThemeToggle();
   
-  const ExampleContent : IHeaderContent = {}
+  // const layoutInstanceRef = useRef<ISplitLayoutHandles>(null);
 
-  const Arrows = <>
-    <Arrow className='t l' style={{transform: 'rotate(-90deg)'}} />
-    <Arrow className='t r' style={{transform: 'rotate(0deg)'}} />
-    <Arrow className='b r' style={{transform: 'rotate(90deg)'}} />
-    <Arrow className='b l' style={{transform: 'rotate(180deg)'}} />
-  </>;
-
-  const layoutInstanceRef = useRef<ISplitLayoutHandles>(null);
-
-  const [layout, setLayout] = useState<'horizontal'|'vertical'>('horizontal');
-  const [reverse, setReverse] = useState<boolean>(false);
-  const areaAContent = <ContentPlaceholder>AREA A{Arrows}</ContentPlaceholder>;
-  const areaBContent = <ContentPlaceholder>AREA B{Arrows}</ContentPlaceholder>;
-  const areaCContent = <ContentPlaceholder>AREA C{Arrows}</ContentPlaceholder>;
+  const [layout] = useState<'horizontal'|'vertical'>('horizontal');
+  const [reverse] = useState<boolean>(false);
+  
+  const areaAContent = <FlexContentPlaceholder title='Area A' />;
+  const areaBContent = <FlexContentPlaceholder title='Area B' />;
 
   return (
       <ThemeProvider theme={defaultTheme}>
@@ -264,9 +194,9 @@ const SplitLayouts: FC = () => {
           ]}
         >
           
-          <ContentLayout layout="dashboard" HeaderContent={ExampleContent}>
+          <ContentLayout layout="dashboard">
             <SplitLayout 
-              ref={layoutInstanceRef}
+              // ref={layoutInstanceRef}
               layout={layout}
               persist 
               persistenceKey='my_unique_key'
