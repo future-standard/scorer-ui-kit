4247a1fb6f2e686598e2a50ab9e2a8ebb0cdd140
Isabel Anguiano <isabel.anguianop@gmail.com>
2024-08-14 20:13:57 +0900

    Update arrow logic in Tooltip

--- packages/ui-lib/src/Alerts/atom/Tooltip.tsx
+++ packages/ui-lib/src/Alerts/atom/Tooltip.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { AlertType } from '..';
 import Icon, { IconWrapper } from '../../Icons/Icon';
 
-const Container = styled.div<{type: AlertType}>`
+const Triangle = styled.div<{type: AlertType}>`
+    content:'';
+    display:block;
+    width:0;
+    height:0;
+    border-left: 8px solid transparent;
+    border-right: 8px solid transparent;
+    border-bottom: 8px solid ${({type}) => `var(--${type})` };
+`;
+
+const Container = styled.div<{directionStyle: string}>`
+  position: absolute;
+  top: 60px;
+  left: 60px;
+  display: flex;
+  ${({directionStyle}) => directionStyle && css`${directionStyle}`};
+`;
+
+const MessageWrapper = styled.div<{type: AlertType}>`
   background-color: ${({type}) => `var(--${type})` };
   border-radius: 3px;
   border: 1px solid var(--grey-a7);
@@ -16,16 +34,12 @@ const Container = styled.div<{type: AlertType}>`
   align-items: center;
   gap: 16px;
 
-  position: absolute;
-  top: 10px;
-  left: 10px;
-
 
   color: var(--white-12);
   font-feature-settings: 'liga' off, 'clig' off;
   font-size: 13px;
   font-weight: 600;
-  line-height: 16px; /* 123.077% */
+  line-height: 16px;
 
   ${IconWrapper} {
     display: flex;
@@ -41,11 +55,95 @@ type ITooltip = {
   type?: AlertType
 }
 
+const getDirectionStyle = (state: ITooltipPosition) => {
+
+  switch (state) {
+    case 'top-start':
+      return `
+              flex-direction: column;
+              ${Triangle} {
+                margin-left: 16px;
+              }
+            `;
+
+    case 'top-center':
+      return `
+              flex-direction: column;
+              align-items: center;
+            `;
+
+    case 'top-end':
+      return `
+              flex-direction: column;
+              align-items: end;
+              ${Triangle} {
+                margin-right: 16px;
+              }
+            `;
+
+    case 'left':
+      return `
+              flex-direction: row;
+              align-items: center;
+              ${Triangle} {
+                transform: rotate(-90deg);
+                margin-right: -4px;
+              }
+            `;
+
+    case 'right':
+      return `
+              flex-direction: row-reverse;
+              align-items: center;
+              ${Triangle} {
+                transform: rotate(90deg);
+                margin-left: -4px;
+              }
+            `;
+
+    case 'bottom-start':
+      return `
+              flex-direction: column-reverse;
+              ${Triangle} {
+                transform: rotate(180deg);
+                margin-left: 16px;
+              }
+            `;
+
+    case 'bottom-end':
+      return `
+              flex-direction: column-reverse;
+              align-items: end;
+              ${Triangle} {
+                transform: rotate(180deg);
+                margin-right: 16px;
+              }
+            `;
+
+    case 'bottom-center':
+
+    // eslint-disable-next-line no-fallthrough
+    default:
+      return `
+              flex-direction: column-reverse;
+              align-items: center;
+              ${Triangle} {
+                transform: rotate(180deg);
+              }
+            `;
+  }
+};
+
+type ITooltipPosition = 'top-start' | 'top-center' | 'top-end' | 'bottom-start' | 'bottom-center' | 'bottom-end' | 'left' | 'right';
+
 const Tooltip: React.FC<ITooltip> = ({icon, message, type}) => {
   return(
-    <Container type={type || 'neutral'}>
-      <Icon icon={icon || ''} size={16}/>
-      {message}
+    <Container directionStyle={getDirectionStyle('top-start')}>
+      <Triangle type={type || 'neutral'}/>
+      <MessageWrapper type={type || 'neutral'}>
+        <Icon icon={icon || ''} size={16}/>
+        {message}
+      </MessageWrapper>
     </Container>
   );
 };
