cc40b122a033ec428a8b215e204ad379b74c4be1
Isabel Anguiano <isabel.anguianop@gmail.com>
2024-08-21 12:09:04 +0900

    Remove last commit css changes to examples

--- packages/example/src/style.ts
+++ packages/example/src/style.ts
@@ -4,17 +4,8 @@ import { BaseStyles } from 'scorer-ui-kit';
 const GlobalStyle = createGlobalStyle`
   ${BaseStyles};
 
-  // Custom changes for examples usage.
-  html, body, #root {
-    height: 100%;
-  }
-
   body {
     background: var(--grey-3);
-  }
-  
-  #root {
-    display: flex;
   }
 `
 export default GlobalStyle;
