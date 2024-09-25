2a19b94b5f231c870c98452344b4027d355e8dd7
Leonard Burton <leonard.burton@futurestandard.co.jp>
2024-09-25 19:35:13 +0900

    Fixed linting error

--- packages/ui-lib/src/Global/organisms/MobileMenu.tsx
+++ packages/ui-lib/src/Global/organisms/MobileMenu.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState, useContext } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { useLocation } from 'react-router-dom';
 import { TabContext, ContextProps } from '../../Tabs/Tabs';
 
