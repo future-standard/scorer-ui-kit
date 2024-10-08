985d8969a6cd535822564990c80df6942421dfab
Leonard Burton <leonard.burton@futurestandard.co.jp>
2024-10-08 16:44:03 +0900

    Button states - requires tidy up

--- packages/ui-lib/src/Form/atoms/Button.tsx
+++ packages/ui-lib/src/Form/atoms/Button.tsx
@@ -1,5 +1,5 @@
 import React, { ButtonHTMLAttributes } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { TypeButtonSizes, TypeButtonDesigns } from '..';
 
 
@@ -8,6 +8,21 @@ interface IStyledComponentProps {
   design: TypeButtonDesigns
 }
 
+const activeAnimation = keyframes`
+  0% {
+    box-shadow: 0 0px 0px var(--white-a8) inset;
+    /* border-color: 1px solid var(--button-hover-border-color); */
+  }
+
+  75% {
+    box-shadow: 0 0 24px var(--white-a9) inset;
+  }
+
+  100% {
+    box-shadow: 0 0 16px var(--white-a8) inset;
+    /* border-color: 1px solid var(--button-active-border-color); */
+  }
+`;
 
 const StyledButton = styled.button<IStyledComponentProps>`
 
@@ -19,7 +34,7 @@ const StyledButton = styled.button<IStyledComponentProps>`
   height: var(--button-height);
   padding: var(--button-h-padding);
   font-size: var(--button-font-size);
-  color: var(--white-12);
+  color: var(--button-text-color);
   font-weight: 600;
 
   overflow: hidden;
@@ -31,13 +46,14 @@ const StyledButton = styled.button<IStyledComponentProps>`
   border-radius: 3px;
   border: 1px solid transparent;
   background: linear-gradient(135deg, transparent, transparent, var(--button-gradient-start), var(--button-gradient-end));
-  background-color: var(--primary-9);
+  background-color: var(--button-background-color);
   background-size: 400%;
   background-position: 99%;
-  box-shadow: 0 0 0 transparent;
+  box-shadow: 0 0 0 var(--button-inner-shadow-color) inset;
   
   transition:
-    background-color var(--speed-normal) var(--easing-primary-out),
+    border-color var(--speed-slow) var(--easing-primary-in-out),
+    background-color var(--speed-normal) var(--easing-primary-in-out),
     background-position var(--speed-normal) var(--easing-primary-out),
     background-size var(--speed-normal) var(--easing-primary-out),
     box-shadow var(--speed-normal) var(--easing-primary-out),
@@ -45,13 +61,18 @@ const StyledButton = styled.button<IStyledComponentProps>`
 
   &:hover:enabled {
     background-position: 1%;
-    background-color: var(--primary-8);
-    border-color: var(--primary-a6);
+    background-color: var(--button-hover-background-color);
+    border-color: var(--button-hover-border-color);
+    /* box-shadow: 0 0 5px var(--button-hover-inner-shadow-color) inset; */
+    color: var(--button-hover-text-color);
   }
 
   &:active:enabled {
-    background-color: var(--primary-7);
-    border-color: var(--primary-a10);
+    /* background-color: var(--button-active-background-color); */
+    /* border-color: var(--button-active-border-color); */
+    /* box-shadow: 0 0 10px var(--white-a10) inset; */
+    color: var(--button-active-text-color);
+    animation: var(--speed-normal) var(--easing-primary-in-out) ${activeAnimation} forwards;
   }
 
   &:disabled {
