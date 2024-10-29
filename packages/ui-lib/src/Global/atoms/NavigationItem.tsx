4d2aa2c67f5d3a7776652903026f19798e63e9a1
Leonard Burton <leonard.burton@futurestandard.co.jp>
2024-10-29 20:25:52 +0900

    Removed unused dependency import

--- packages/ui-lib/src/Global/atoms/NavigationItem.tsx
+++ packages/ui-lib/src/Global/atoms/NavigationItem.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import {Link} from 'react-router-dom';
 import ContextItem from './ContextItem';
