e386aa0df8a786150b0e2e3f285e1c86ca594a54
Isabel Anguiano <isabel.anguianop@gmail.com>
2024-11-18 11:29:30 +0900

    Verified that typescript workaround is not longer needed in storybok8

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -3,9 +3,6 @@
 module.exports = {
   framework: {
     name: "@storybook/react-webpack5",
-    options: {
-      builder: { lazyCompilation: true },
-    },
   },
   stories: [
     '../src/stories/Global/*.stories.tsx',
@@ -18,17 +15,5 @@ module.exports = {
     '@storybook/addon-knobs',
     'storybook-dark-mode/register',
     'storybook-addon-jsx/register',
-  ],
-  // This is because of a typescript error that is probably fix in the next version of storybook
-  // should try to delete when update
-  // https://github.com/styleguidist/react-docgen-typescript/issues/356#issuecomment-857887751
-  typescript: {
-    reactDocgen: 'react-docgen',
-  },
-
-  // Up until version 7.0, it was possible to generate the default export of a CSF story by calling a function, or mixing in variables defined in other ES Modules. For instance:  //
-  // To opt-out of the old behavior you can set the storyStoreV7 feature flag to false in main.js.
-  features: {
-    storyStoreV7: false,
-  },
-};
+  ]
+}
