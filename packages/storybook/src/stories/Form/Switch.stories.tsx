1e2f812f8d6fe93f575abe2201554104b084c13a
Isabel Anguiano <isabel.anguianop@gmail.com>
2025-03-12 11:25:09 +0900

    Add usecase to Switch to cover both controlled and uncontrolled scenarios

--- packages/storybook/src/stories/Form/Switch.stories.tsx
+++ packages/storybook/src/stories/Form/Switch.stories.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { action } from '@storybook/addon-actions';
 import { text, select, boolean } from "@storybook/addon-knobs";
-import {Switch} from 'scorer-ui-kit';
+import {Switch, TypeSwitchState} from 'scorer-ui-kit';
+import { sleep } from '../helpers';
 
 export default {
   title: 'Form/atoms',
@@ -11,12 +12,34 @@ export default {
 
 export const _Switch = () => {
 
+  const [checkValue, setCheckValue] = useState(false);
+  const [switchState, setSwitchState] = useState<TypeSwitchState>('default');
+
   const labelText = text("Label Text", "The Label");
-  const checked = boolean("Default Checked", true)
-  const state = select("State", { Default: "default", Disabled: "disabled", Locked: "locked", Loading: "loading", Failure: "failure" }, "default");
+  const knobCheck = boolean("Check", false)
+  const knobState = select("State", { Default: "default", Disabled: "disabled", Locked: "locked", Loading: "loading", Failure: "failure"}, "default") as TypeSwitchState;
   const leftTheme = select("Left Theme", { Off: "off", On: "on", Danger: "danger" }, "off");
   const rightTheme = select("Right Theme", { Off: "off", On: "on", Danger: "danger" }, "on");
   const onChangeCallback = action('value-changed');
+  const uncontrolled = boolean('Work as self controlled Component', true);
 
-  return <Switch {...{state, leftTheme, rightTheme, labelText, checked, onChangeCallback}} />;
-};
+  const failProcessTest = useCallback(async (value) => {
+    console.log('This test is meant to fail, input should be check false');
+    console.log('Switch input value', value);
+    setSwitchState('loading');
+    await sleep(1200);
+    setCheckValue(false);
+    setSwitchState('default');
+    onChangeCallback(value);
+  },[onChangeCallback]);
+
+  useEffect(() => {
+    setCheckValue(knobCheck);
+  },[knobCheck])
+
+  useEffect(() => {
+    setSwitchState(knobState);
+  },[knobState])
+
+  return <Switch {...{state: switchState, leftTheme, rightTheme, labelText, onChangeCallback: uncontrolled ? onChangeCallback : failProcessTest, checked: uncontrolled ? undefined : checkValue}} />;
+}
