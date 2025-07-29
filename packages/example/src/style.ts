1be54e7f297a36bc8de3ee32f5dc2d46bfba8fe4
Isabel Anguiano <isabel.anguianop@gmail.com>
2025-07-29 11:58:40 +0900

    Fix missing semicol missing

--- packages/example/src/style.ts
+++ packages/example/src/style.ts
@@ -1,7 +1,7 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 import { BaseStyles } from 'scorer-ui-kit';
 
 const GlobalStyle = createGlobalStyle`
   ${BaseStyles};
-`
+`;
 export default GlobalStyle;
