bfcd7dd8504a1dd7e3540ea8907fd5062bbd4cc2
Isabel Anguiano <isabel.anguianop@gmail.com>
2026-04-09 00:13:17 -0600

    Create React App artifacts removal (#629)
    * Removal of the artifacts created by Create React APP
    
    * Update examples read me to remove create react app note and add more useful notes

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -9,7 +9,6 @@ import { defaultTheme,
         } from 'scorer-ui-kit';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import './i18n';
 import Fonts from './fonts';
@@ -89,8 +88,3 @@ root.render(
     <Index />
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
