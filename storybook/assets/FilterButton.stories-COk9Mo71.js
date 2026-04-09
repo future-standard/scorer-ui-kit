6b9253126d97e9bad2eb79b55242acdbb4617c30
Isabel Anguiano <isabel.anguianop@gmail.com>
2026-04-09 14:55:17 +0900

    Updates

---
+++ storybook/assets/FilterButton.stories-COk9Mo71.js
@@ -0,0 +1,22 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{C as t,Et as n,Ft as r,Lt as i,Pt as a,Rt as o,bt as s}from"./iframe-DufxECJ3.js";import{n as c,r as l}from"./helpers-Ds3vW7k8.js";var u,d,f,p,m,h;e((()=>{o(),u=a(),s(),r(),l(),d=n(),f={title:`Filters/atoms`,component:t,decorators:[]},p=i.div``,m=()=>{let e=c(),n=(0,u.text)(`Text`,`Search Example String Field`);return(0,d.jsx)(p,{children:(0,d.jsx)(t,{icon:(0,u.select)(`Icon`,e,Object.keys(e)[0]),hasFlipArrow:(0,u.boolean)(`Has Flip Arrow`,!1),isOpen:(0,u.boolean)(`is Open`,!1),disabled:(0,u.boolean)(`disable`,!1),design:(0,u.select)(`Design type`,{Default:`default`,Basic:`basic`},`default`),children:n})})},m.__docgenInfo={description:``,methods:[],displayName:`_FilterButton`},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`() => {
+  const iconList = generateIconList();
+  const buttonText = text('Text', 'Search Example String Field');
+  const iconName = select("Icon", iconList, Object.keys(iconList)[0]);
+  const hasFlipArrow = boolean('Has Flip Arrow', false);
+  const isOpen = boolean('is Open', false);
+  const disabled = boolean('disable', false);
+  const design = select('Design type', {
+    Default: 'default',
+    Basic: 'basic'
+  }, 'default');
+  return <Content>
+    <FilterButton icon={iconName} {...{
+      hasFlipArrow,
+      isOpen,
+      disabled,
+      design
+    }}>
+      {buttonText}
+    </FilterButton>
+  </Content>;
+}`,...m.parameters?.docs?.source}}},h=[`_FilterButton`]}))();export{m as _FilterButton,h as __namedExportsOrder,f as default};
---
+++ storybook/assets/TableHeaderTitle.stories-0w9qjO1_.js
@@ -0,0 +1,25 @@
+import{a as e,n as t}from"./chunk-BneVvdWh.js";import{Et as n,Ft as r,Lt as i,Pt as a,Rt as o,bt as s,st as c}from"./iframe-DufxECJ3.js";var l,u,d,f,p,m,h;t((()=>{l=e(o()),r(),u=a(),s(),d=n(),f={title:`Tables/atoms`,component:c,decorators:[]},p=i.div`
+  font-family: ${e=>e.theme.fontFamily.data};
+  display: table-cell;
+  height: inherit;
+  vertical-align:top;
+  line-height: 20px;
+  position: relative;
+  margin: 100px;
+`,m=()=>{let[e,t]=(0,l.useState)(!0),n=(0,u.boolean)(`isSortActive`,!0),r=(0,u.boolean)(`sortable`,e),i=(0,u.boolean)(`ascending`,e),a=(0,u.text)(`header`,`Device Name`);return(0,l.useEffect)(()=>{t(i)},[i]),(0,d.jsx)(p,{children:(0,d.jsx)(c,{sortable:r,isSortActive:n,ascending:e,columnId:`firstColumn`,indexKey:1,header:a,toggleSort:(0,l.useCallback)(n=>{console.log(`arriving`,e),t(e=>!e)},[e])})})},m.__docgenInfo={description:``,methods:[],displayName:`_TableHeaderTitle`},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`() => {
+  const [toggleSort, setToggleSort] = useState(true);
+  const isSortActive = boolean('isSortActive', true);
+  const sortable = boolean('sortable', toggleSort);
+  const asc = boolean('ascending', toggleSort);
+  const header = text('header', 'Device Name');
+  useEffect(() => {
+    setToggleSort(asc);
+  }, [asc]);
+  const sortHandler = useCallback((indexKey: number) => {
+    console.log('arriving', toggleSort);
+    setToggleSort(prev => !prev);
+  }, [toggleSort]);
+  return <Container>
+      <TableHeaderTitle sortable={sortable} isSortActive={isSortActive} ascending={toggleSort} columnId={'firstColumn'} indexKey={1} header={header} toggleSort={sortHandler} />
+  </Container>;
+}`,...m.parameters?.docs?.source}}},h=[`_TableHeaderTitle`]}))();export{m as _TableHeaderTitle,h as __namedExportsOrder,f as default};
